b9827d1f549b8350f68a0bbc396602b0caff3cf7
Scott Collis <scollis.acrf@gmail.com>
2014-09-22 19:20:45 -0500

    ADD: KAZR

--- kazr.py
+++ kazr.py
@@ -55,7 +55,7 @@ for i in range(len(files)):
     dview = c[:]
     dview.block = False
     dview.execute('import pyart')
-    dview.execute('from netCDF4 import num2date')
+    dview.execute('import netCDF')
     print files[i]
     result = dview.map_async(get_date,files[i])
     datestrs = result.get()
